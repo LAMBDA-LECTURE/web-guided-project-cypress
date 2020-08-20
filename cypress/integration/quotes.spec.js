bb9160096e12e119eadb7dc173d877c263165392
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-20 18:24:48 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -5,7 +5,7 @@ function foo(params) {
 // write tests here
 describe('Quotes App', () => {
   describe('Inputs and submit button', () => {
-    test('Returns the string "foo"', () => {
+    it('Returns the string "foo"', () => {
       expect(foo()).to.eq('foo')
       expect(foo()).not.to.eq('bar')
     })
