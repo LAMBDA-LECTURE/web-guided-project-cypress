e67ea205d09ce6ff57d74151cf0ba9ab7c6900a4
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-20 18:37:00 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -3,7 +3,7 @@ describe('Quotes App', () => {
   describe('Inputs and submit button', () => {
     it('can navigate to http://localhost:1234', () => {
       cy.visit('http://localhost:1234')
-      cy.url().should('include', 'foo')
+      cy.url().should('include', 'localhost')
     })
   })
 })
